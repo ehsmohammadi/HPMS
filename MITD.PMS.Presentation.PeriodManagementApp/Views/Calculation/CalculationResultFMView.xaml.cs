18bb7a4f7f7f0938cc6fcd714cc3c8d512458d56
ehsmohammadi <ehs.mohammadi@gmail.com>
2016-10-13 16:18:08 +0330

    ManagerViewCalculation Result was added

---
+++ MITD.PMS.Presentation.PeriodManagementApp/Views/Calculation/CalculationResultFMView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MITD.Presentation.UI;
+using MITD.PMS.Presentation.Logic;
+
+
+
+namespace MITD.PMS.Presentation.PeriodManagementApp.Views
+{
+    public partial class CalculationResultFMView : ViewBase, ICalculationResultView
+    {
+        public CalculationResultFMView()
+        {
+            InitializeComponent();
+        }
+
+        public CalculationResultFMView(CalculationResultVM vm)
+        {
+            InitializeComponent();
+            ViewModel = vm;
+
+        }
+    }
+}
